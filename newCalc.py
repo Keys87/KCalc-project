f0026af7b52879ad3ee790eff3bdb9d2cf87f7e6
Keys87 <119033902+Keys87@users.noreply.github.com>
2023-03-25 22:51:24 +0700

    Add files via upload

---
+++ newCalc.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+textbox_x = tk.Entry()
+textbox_y = tk.Entry()
+tkinter_result = tk.IntVar()
+tkinter_sign = tk.StringVar()
+label_sign = tk.Label(textvariable=tkinter_sign)
+label_result = tk.Label(textvariable=tkinter_result)
+
+
+def add_onclick():
+    x = int(textbox_x.get())
+    y = int(textbox_y.get())
+    sign = "+"
+    tkinter_sign.set(sign)
+    result = x + y
+    tkinter_result.set(result)
+
+def sub_onclick():
+    x = int(textbox_x.get())
+    y = int(textbox_y.get())
+    sign = "-"
+    tkinter_sign.set(sign)
+    result = x - y
+    tkinter_result.set(result)
+
+
+def mtpl_onclick():
+    x = int(textbox_x.get())
+    y = int(textbox_y.get())
+    sign = "x"
+    tkinter_sign.set(sign)
+    result = x * y
+    tkinter_result.set(result) 
+ 
+
+def div_onclick():
+    x = int(textbox_x.get())
+    y = int(textbox_y.get())
+    sign = ":"
+    tkinter_sign.set(sign)
+    result = x / y
+    tkinter_result.set(result)
+        
+
+button_add = tk.Button(text="+", command=add_onclick)
+button_sub = tk.Button(text="-", command=sub_onclick)
+button_mtlp = tk.Button(text="x", command=mtpl_onclick)
+button_div = tk.Button(text=":", command=div_onclick)
+textbox_x.grid(column=1, row=1)
+label_sign.grid(column=2, row=1)
+textbox_y.grid(column=3, row=1)
+label_result.grid(column=4, row=1)
+button_sub.grid(column=1, row=3)
+button_add.grid(column=1, row=3)
+
+root.mainloop()
