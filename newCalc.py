3c8d7a6537203c0ce2ee5acb9c4fd1b31a3f1aa0
Keys87 <119033902+Keys87@users.noreply.github.com>
2023-03-27 10:39:58 +0700

    Add files via upload

--- newCalc.py
+++ newCalc.py
@@ -9,41 +9,59 @@ tkinter_sign = tk.StringVar()
 label_sign = tk.Label(textvariable=tkinter_sign)
 label_result = tk.Label(textvariable=tkinter_result)
 
-x = textbox_x.get()
-y = textbox_y.get()
-if x == " ":
-    x = int(textbox_x.get())
 
-if y == " ":
-    y = int(textbox_y.get())
+def value_checker():
+    x = textbox_x.get()
+    y = textbox_y.get()
+    if x != " ":
+        x = int(textbox_x.get())
+        return x
+    if y != " ":
+        y = int(textbox_y.get())
+        return y
 
 
 def add_onclick():
-    sign = "+"
-    tkinter_sign.set(sign)
-    result = x + y
-    tkinter_result.set(result)
+    def main_event(local_x, local_y):
+        sign = "+"
+        tkinter_sign.set(sign)
+        result = local_x + local_y
+        tkinter_result.set(result)
+    
+    value_checker()
+    main_event()
 
-def sub_onclick():
-    sign = "-"
-    tkinter_sign.set(sign)
-    result = x - y
-    tkinter_result.set(result)
+def sub_onclick(local_x, local_y):
+    def main_event():
+        sign = "-"
+        tkinter_sign.set(sign)
+        result = local_x - local_y
+        tkinter_result.set(result)
+        
+    value_checker()
+    main_event()
 
 
-def mtpl_onclick():
-    sign = "x"
-    tkinter_sign.set(sign)
-    result = x * y
-    tkinter_result.set(result) 
+def mtpl_onclick(local_x, local_y):
+    def main_event():
+        sign = "x"
+        tkinter_sign.set(sign)
+        result = local_x * local_y
+        tkinter_result.set(result) 
+     
+    value_checker()
+    main_event()   
  
 
-def div_onclick():
-    sign = ":"
-    tkinter_sign.set(sign)
-    result = x / y
-    tkinter_result.set(result)
+def div_onclick(local_x, local_y):
+	def main_event():
+        sign = ":"
+        tkinter_sign.set(sign)
+        result = local_x / local_y
+        tkinter_result.set(result)
         
+    value_checker()
+    main_event()
 
 button_add = tk.Button(text="+", command=add_onclick)
 button_sub = tk.Button(text="-", command=sub_onclick)
@@ -53,8 +71,10 @@ textbox_x.grid(column=1, row=1)
 label_sign.grid(column=2, row=1)
 textbox_y.grid(column=3, row=1)
 label_result.grid(column=4, row=1)
-button_sub.grid(column=1, row=3)
+button_sub.grid(column=2, row=3)
 button_add.grid(column=1, row=3)
+button_div.grid(column=4, row=3)
+button_mtlp.grid(column=3, row=3)
 
 
 root.mainloop()
