2e6909909a6bbcba2f93dc000b6a4bf95b5e309f
Keys87 <119033902+Keys87@users.noreply.github.com>
2023-03-28 19:57:49 +0700

    Update main[V3.0].py

--- main[V3.0].py
+++ main[V3.0].py
@@ -4,35 +4,17 @@ import math as m
 
 root = tk.Tk()
 textbox_x = tk.Entry(relief="solid")
-textbox_y = tk.Entry(relief="solid")
+textbox_y = tk.Entry(relief="solid", state="normal")
 tkinter_result = tk.DoubleVar()
 tkinter_sign = tk.StringVar()
+tkinter_textbox_y_state = tk.StringVar()
 label_sign = tk.Label(textvariable=tkinter_sign)
 label_result = tk.Label(textvariable=tkinter_result)
 label_equal_sign = tk.Label(text="=")
 
 
-def value_checker():
-    x = textbox_x.get()
-    y = textbox_y.get()
-    if x != " ":
-        x = float(textbox_x.get())
-        return x
-
-    elif x == " ":
-        x = 0.0
-        return x
-
-    if y != " ":
-        y = float(textbox_y.get())
-        return y
-
-    elif y == " ":
-        y = 0.0
-        return y
-
-
 def add_onclick():
+    tkinter_textbox_y_state.set("normal")
     x = textbox_x.get()
     y = textbox_y.get()
     if x != " ":
@@ -49,8 +31,8 @@ def add_onclick():
 
     def main_event():
         sign = "+"
-        tkinter_sign.set(sign)
         result = x + y
+        tkinter_sign.set(sign)
         tkinter_result.set(result)
 
     main_event()
@@ -58,6 +40,7 @@ def add_onclick():
 
 def sub_onclick():
     def main_event():
+        tkinter_textbox_y_state.set("normal")
         sign = "-"
         tkinter_sign.set(sign)
         result = x - y
@@ -82,6 +65,7 @@ def sub_onclick():
 
 def mtpl_onclick():
     def main_event():
+        tkinter_textbox_y_state.set("normal")
         sign = "x"
         tkinter_sign.set(sign)
         result = x * y
@@ -106,6 +90,7 @@ def mtpl_onclick():
 
 def div_onclick():
     def main_event():
+        tkinter_textbox_y_state.set("normal")
         sign = ":"
         tkinter_sign.set(sign)
         result = x / y
@@ -130,6 +115,7 @@ def div_onclick():
 
 def pwr_onclick():
     def main_event():
+        tkinter_textbox_y_state.set("normal")
         sign = "^"
         tkinter_sign.set(sign)
         result = m.pow(x, y)
@@ -150,9 +136,28 @@ def pwr_onclick():
         y = 0.0
 
     main_event()
+    
+def sqrt_onclick():
+    def main_event():
+        x = textbox_x.get()
+        
+        if x != " ":
+            x = float(textbox_x.get())
+
+        elif x == " ":
+            x = 0.0
+    	
+        tkinter_textbox_y_state.set("readonly")
+        sign = "√"
+        tkinter_sign.set(sign)
+        result = m.sqrt(x)
+        tkinter_result.set(result)
 
 
-font_for_buttons = font.Font(family="calibri", size=11)
+    main_event()
+
+
+font_for_buttons = font.Font(family="calibri", size=11, weight="bold")
 
 
 button_add = tk.Button(text="+", command=add_onclick, relief="solid", bd=1, width=6)
@@ -160,15 +165,7 @@ button_sub = tk.Button(text="-", command=sub_onclick, relief="solid", bd=1, widt
 button_mtpl = tk.Button(text="x", command=mtpl_onclick, relief="solid", bd=1, width=6)
 button_div = tk.Button(text=":", command=div_onclick, relief="solid", bd=1, width=6)
 button_pwr = tk.Button(text="^", command=pwr_onclick, relief="solid", bd=1, width=6)
-button_add["font"] = font_for_buttons
-button_sub["font"] = font_for_buttons
-button_mtpl["font"] = font_for_buttons
-button_div["font"] = font_for_buttons
-textbox_x["font"] = font_for_buttons
-textbox_y["font"] = font_for_buttons
-label_sign["font"] = font_for_buttons
-label_result["font"] = font_for_buttons
-label_equal_sign["font"] = font_for_buttons
+button_sqrt  = tk.Button(text="√", command=sqrt_onclick, relief="solid", bd=1, width=6)
 
 
 textbox_x.grid(column=1, row=1)
@@ -181,5 +178,6 @@ button_sub.grid(column=2, row=2)
 button_mtpl.grid(column=3, row=2)
 button_div.grid(column=4, row=2)
 button_pwr.grid(column=1, row=3)
+button_sqrt.grid(column=2, row=3)
 
 root.mainloop()
